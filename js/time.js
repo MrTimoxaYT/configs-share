3dff0047376d54a4246756ead58770fa21a45d70
MrTimoxaYT <84664691+MrTimoxaYT@users.noreply.github.com>
2024-02-05 12:12:38 +0300

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,7 +1,5 @@
 var time_is_widget = new time_is_widget()
 function time_is_widget() {
-    // Найдите и замените "Россия" на "Москва" в коде шаблона
-    a["template"] = ct.replace("SUN", tl + sF + "</span>").replace("TIME", tl + tF + "</span>").replace("DATE", '<span onclick="window.location=\'https://time.is/calendar\'" title="https://time.is/calendar">' + dF + '</span>').replace("Россия", "Москва");
 
     var ca = 0, tD = 0, tout = 0, updint = 1000, tl = "", U = "undefined", i, j, rd
     var p = { n: ["%D0%B2%D0%BE%D1%81%D0%BA%D1%80%D0%B5%D1%81%D0%B5%D0%BD%D1%8C%D0%B5.%D0%BF%D0%BE%D0%BD%D0%B5%D0%B4%D0%B5%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA.%D0%B2%D1%82%D0%BE%D1%80%D0%BD%D0%B8%D0%BA.%D1%81%D1%80%D0%B5%D0%B4%D0%B0.%D1%87%D0%B5%D1%82%D0%B2%D0%B5%D1%80%D0%B3.%D0%BF%D1%8F%D1%82%D0%BD%D0%B8%D1%86%D0%B0.%D1%81%D1%83%D0%B1%D0%B1%D0%BE%D1%82%D0%B0.%D0%B2%D1%81.%D0%BF%D0%BD.%D0%B2%D1%82.%D1%81%D1%80.%D1%87%D1%82.%D0%BF%D1%82.%D1%81%D0%B1.%D1%8F%D0%BD%D0%B2%D0%B0%D1%80%D1%8C.%D1%84%D0%B5%D0%B2%D1%80%D0%B0%D0%BB%D1%8C.%D0%BC%D0%B0%D1%80%D1%82.%D0%B0%D0%BF%D1%80%D0%B5%D0%BB%D1%8C.%D0%BC%D0%B0%D0%B9.%D0%B8%D1%8E%D0%BD%D1%8C.%D0%B8%D1%8E%D0%BB%D1%8C.%D0%B0%D0%B2%D0%B3%D1%83%D1%81%D1%82.%D1%81%D0%B5%D0%BD%D1%82%D1%8F%D0%B1%D1%80%D1%8C.%D0%BE%D0%BA%D1%82%D1%8F%D0%B1%D1%80%D1%8C.%D0%BD%D0%BE%D1%8F%D0%B1%D1%80%D1%8C.%D0%B4%D0%B5%D0%BA%D0%B0%D0%B1%D1%80%D1%8C"], w: "%D0%BD%D0%B5%D0%B4%D0%B5%D0%BB%D1%8F ", W: "%D0%BD%D0%B5%D0%B4%D0%B5%D0%BB%D1%8F .n", dy: " %D0%B4%D0%B5%D0%BD%D1%8C" }
